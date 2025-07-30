b9fc35f18dea243b4abfc90e097f5ffb83c1e578
Borko Livić <borkolivic@gmail.com>
2025-07-30 12:43:44 +0200

    Update admin-bulk-modal.js

--- includes/js/admin-bulk-modal.js
+++ includes/js/admin-bulk-modal.js
@@ -1,14 +1,12 @@
 jQuery(function($){
   const EXPECTED = ['scu name','products','coupon'];
 
-  // EXPORT: direct CSV download
   $('#scu-export-btn').on('click', function(e){
     e.preventDefault();
     const url = `${SCU_Ajax.ajax_url}?action=scu_export_csv&_wpnonce=${SCU_Ajax.export_nonce}`;
     window.location = url;
   });
 
-  // Header validation (strips wrapping quotes)
   function validateHeader(file){
     const reader = new FileReader();
     reader.onload = function(e){
@@ -27,7 +25,6 @@ jQuery(function($){
     reader.readAsText(file.slice(0,200));
   }
 
-  // Drag & drop zone
   $('#scu-dropzone')
     .on('dragover', function(e){
       e.preventDefault();
@@ -42,14 +39,12 @@ jQuery(function($){
       }
     });
 
-  // File‐picker change
   $('#scu-import-file').on('change', function(){
     if ( this.files.length ) {
       validateHeader(this.files[0]);
     }
   });
 
-  // IMPORT: queue up processing
   $('#scu-import-submit').on('click', function(){
     const inp = $('#scu-import-file')[0];
     if ( ! inp.files.length ) {
@@ -61,7 +56,6 @@ jQuery(function($){
     fd.append('_wpnonce', SCU_Ajax.import_nonce);
     fd.append('scu_csv', inp.files[0]);
 
-    // reset UI
     $('#scu-import-result').text('Queuing…');
     $('#scu-import-progress').show().val(0);
 
@@ -82,7 +76,6 @@ jQuery(function($){
     });
   });
 
-  // Poll server for progress
   function pollProgress(job){
     $.getJSON(SCU_Ajax.ajax_url, {
       action: 'scu_import_progress',
@@ -96,7 +89,6 @@ jQuery(function($){
       const pct = d.total ? Math.round(100 * d.completed / d.total) : 0;
       $('#scu-import-progress').val(pct);
 
-      // Show “X of Y processed…”
       $('#scu-import-result').text(
         `${d.completed} of ${d.total} processed…`
       );
@@ -104,17 +96,13 @@ jQuery(function($){
       if ( ! d.done ) {
         setTimeout(function(){ pollProgress(job); }, 1000);
       } else {
-        // final message
         $('#scu-import-result').html('<pre>' + d.message + '</pre>');
         $('#scu-import-progress').hide();
 
-        // after ~2.5s, close modal & reload CPT list
         setTimeout(function(){
-          // close Thickbox
           if ( typeof tb_remove === 'function' ) {
             tb_remove();
           }
-          // reload the page
           location.reload();
         }, 2500);
       }
